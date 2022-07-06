c535b725ca913728788a2fd5c7061072bec415bc
Senz404 <108679988+Senz404@users.noreply.github.com>
2022-07-06 13:15:28 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,8 @@
 let handler = async m => m.reply(`
 ╭─・[ Donasi ]
 │・ [ 083800930156 ]
+│・ [ Gopay / Dana ]
+│・ [ Donasi Seiklasnya ]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
